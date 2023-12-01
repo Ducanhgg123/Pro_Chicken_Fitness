5f8d7a3153d60a4e2bab90770b06ed52d30ea2dd
Duc Anh <letrongducanh456@gmail.com>
2023-12-01 21:34:38 +0700

    cors

--- prochickenfitness/src/main/java/com/prochicken/prochickenfitness/Security/SecurityConfig.java
+++ prochickenfitness/src/main/java/com/prochicken/prochickenfitness/Security/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -25,6 +27,19 @@ public class SecurityConfig {
     public SecurityConfig(MyUserDetailService myUserDetailService, JwtRequestFilter jwtRequestFilter) {
         this.myUserDetailService = myUserDetailService;
         this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:3000","http://localhost:5173")
+                        .allowedMethods("*");
+            }
+        };
     }
 
     @Bean
@@ -42,6 +57,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> corsConfigurer());
         http.authorizeHttpRequests(configurer ->
                         configurer
                                 .requestMatchers("/api/authentication/login").permitAll()
