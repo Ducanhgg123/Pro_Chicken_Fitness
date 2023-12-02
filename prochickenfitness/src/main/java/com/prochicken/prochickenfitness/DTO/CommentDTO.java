f0efb0d69ea48ee4dcac477354084c3023b091e7
Duc Anh <letrongducanh456@gmail.com>
2023-12-02 12:31:49 +0700

    asddsa

--- prochickenfitness/src/main/java/com/prochicken/prochickenfitness/DTO/CommentDTO.java
+++
@@ -1,9 +0,0 @@
-package com.prochicken.prochickenfitness.DTO;
-
-import lombok.Data;
-
-@Data
-public class CommentDTO {
-    private Integer id;
-    private String content;
-}
--- prochickenfitness/src/main/java/com/prochicken/prochickenfitness/Service/PostService.java
+++
@@ -1,7 +0,0 @@
-package com.prochicken.prochickenfitness.Service;
-
-import com.prochicken.prochickenfitness.entity.UserEntity;
-
-public interface PostService {
-    UserEntity getUserByPostId(int id);
-}
